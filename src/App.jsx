1302d9ebf5da9f3346261402aa2530011ab9b0c7
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-10-09 06:41:47 +0100

    text update

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className=" w-screen flex items-center justify-center p-2 bg-[#F8F8F8]">
-      <div className="flex flex-col gap-10 lg:gap-10 max-w-3xl ">
+      <div className="flex flex-col gap-10 lg:gap-10 max-w-xl ">
         <div className="flex items-center justify-between py-4">
-          <h1 className="underline text-[2.3rem] font-['Space_Mono'] font-[700]">
+          <h1 className="underline text-4xl lg:text-5xl font-['Space_Mono'] font-[700]">
             FONT-PAIR
           </h1>
           <div className=" flex items-center pr-4">
@@ -13,31 +13,33 @@ function App() {
             <div className="w-6 h-6 rounded-full bg-[#F8F8F8] relative "></div>
           </div>
         </div>
-        <div className=" flex flex-col gap-8 lg:gap-6">
+        <div className=" flex flex-col gap-8 lg:gap-12">
           <div className=" text-center">
-            <div className=" p-4 py-2 bg-[#2F2F2F] text-white text-[1rem] w-max  font-['Space_Mono'] italic">
+            <div className=" p-4 py-2 bg-[#2F2F2F] text-white text-sm lg:text-lg w-max  font-['Space_Mono'] italic">
               <p>libre franklin</p>
             </div>
 
             <h2
               role="textarea"
-              className="resize w-full bg-[#F1F5F9] p-2 lg:p-4  font-['Libre_Franklin'] text-[1.8rem] text-left font-bold"
+              className="resize w-full bg-[#F1F5F9] p-2 lg:p-4  font-['Libre_Franklin'] text-2xl lg:text-4xl text-left font-bold"
               contentEditable
             >
               Font pairing made simple
             </h2>
           </div>
           <div className="">
-            <div className=" p-4 py-2 bg-[#2F2F2F] text-white text-[1rem] w-max font-['Space_Mono'] italic">
+            <div className=" p-4 py-2 bg-[#2F2F2F] text-white  text-sm lg:text-lg w-max font-['Space_Mono'] italic">
               <p>libre baskerville</p>
             </div>
-            <p className="text-left flex flex-col gap-6 p-3 bg-[#F1F5F9] lg:p-6 font-['Libre_Baskerville']">
+            <p className="text-left lg:text-lg flex flex-col gap-6 p-3 bg-[#F1F5F9] lg:p-6 font-['Libre_Baskerville']">
               {/* (Lock) to lock fonts that you want to keep, and (Edit) to choose a
               font manually */}
               <span role="textarea" className="resize w-full" contentEditable>
-                Click (Generate) to create a new font pairing. The text is
-                editable, and so is the heading, try replacing it with your
-                company name or other copy.
+                The goal of font pairing is to select fonts that share an
+                overarching theme yet have a pleasing contrast. Click (Generate)
+                to create a new font pairing. The text is editable, and so is
+                the heading, try replacing it with your company name or other
+                copy.
               </span>
 
               <span role="textarea" className="resize w-full" contentEditable>
@@ -46,10 +48,6 @@ function App() {
                 your web projects when you find a perfect pair.
               </span>
               {/* submit a font pair */}
-              <span role="textarea" className="resize w-full" contentEditable>
-                The goal of font pairing is to select fonts that share an
-                overarching theme yet have a pleasing contrast.
-              </span>
             </p>
           </div>
           <button className="w-full p-4 bg-[#2F2F2F] text-white text-[1rem] font-['Space_Mono']">
